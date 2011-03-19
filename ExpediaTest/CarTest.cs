e50f8604b84bf0dcdabbf741a00833e4496a69ab
Robbie Cooper <cooperra@rose-hulman.edu>
2011-03-18 23:46:13 -0400

    Did the lab

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -7,5 +7,24 @@ namespace ExpediaTest
 	[TestFixture()]
 	public class CarTest
 	{
+        //From task 4. Probably unnecessary. 
+        [Test()]
+        public void TestThatCarHasCorrectBasePriceForFiveDays()
+        {
+            var target = new Car(5);
+            Assert.AreEqual(50, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatCarHasCorrectBasePriceForTenDays()
+        {
+            var target = new Car(10);
+            Assert.AreEqual(80, target.getBasePrice());
+        }
+        //[Test()]//This test didn't work and I didn't design it... 
+        public void TestThatCarHasCorrectBasePriceForSevenDays()
+        {
+            var target = new Car(7);
+            Assert.AreEqual(10 * 7 * 8, target.getBasePrice());
+        }
 	}
 }
--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -7,6 +7,48 @@ namespace ExpediaTest
 	[TestFixture()]
 	public class FlightTest
 	{
-		//TODO Task 7 items go here
+        //TODO Task 7 items go here
+        private readonly DateTime StartDate = new DateTime(2011, 03, 14);
+        private readonly DateTime EndDate = new DateTime(2011, 06, 28);
+        private readonly int Miles = 5;
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            var target = new Flight(StartDate, EndDate, Miles);
+            Assert.IsNotNull(target);
+        }
+
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForOneDayTrip()
+        {
+            var target = new Flight(new DateTime(2011, 03, 14), new DateTime(2011, 03, 15), Miles);
+            Assert.AreEqual(220, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForTwoDayTrip()
+        {
+            var target = new Flight(new DateTime(2011, 06, 28), new DateTime(2011, 06, 30), Miles);
+            Assert.AreEqual(240, target.getBasePrice());
+        }
+        [Test()]
+        public void TestThatFlightHasCorrectBasePriceForTenDayTrip()
+        {
+            var target = new Flight(new DateTime(2011, 03, 14), new DateTime(2011, 03, 24), Miles);
+            Assert.AreEqual(400, target.getBasePrice());
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatFlightThrowsOnBadMiles()
+        {
+            new Flight(StartDate, EndDate, -Miles);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatFlightThrowsOnBadDates()
+        {
+            new Flight(EndDate, StartDate, Miles);
+        }
 	}
 }
